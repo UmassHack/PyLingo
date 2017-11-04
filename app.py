3d78ca4c48ed5d78a0a4ad23ccf2863d7b5ba15a
Nathan L <nlach0629@gmail.com>
2017-11-04 04:31:36 -0400

    Support for googletrans library

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import time
+from googletrans import Translator
 from flask import Flask, render_template, flash, session
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
 @app.route("/")
 def main():
+    translator = Translator()
     file = open("chat.txt", "r")
     line = file.readline();
     while line:
-        flash(line)
+        flash(translator.translate(line, dest='fr').text)
         # time.sleep(5);
         line = file.readline()
     file.close();
