e942b96be1c9f590030041d09ed6692029e2d8e2
Nathan L <nlach0629@gmail.com>
2017-11-04 17:49:30 -0400

    Button now switches microphone listening on and off

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 import time
+import sys
+import ServerSentEvent as sse
+import SpeechToText as st
 from googletrans import Translator
-from flask import Flask, render_template, flash, session
+from flask import Flask
+import flask as f
+from flask import render_template, flash, session, Response
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
+# speechRecognizer = st.SpeechToText()
+# listen = False
+# text = None
+
 @app.route("/")
 def main():
+    # text = speechRecognizer.listen()
     return render_template("index.html")
 
 @app.route("/language/<language>")
 def doTranslation(language):
     translator = Translator()
-    file = open("chat.txt", "r")
     line = file.readline()
     finalString = ""
     language = language.lower()
@@ -23,16 +32,22 @@ def doTranslation(language):
         elif language == "english":
             finalString += translator.translate(line, dest='en').text
         line = file.readline()
-    file.close()
     return finalString
 
-@app.route("/blah/<data>")
-def blah(data):
-    return data
+@app.route("/chatData/<boolean>")
+def sendChatData(boolean):
+    def gen():
+        try:
+            speechRecog = st.SpeechToText()
+            if boolean == "True":
+                speechRecog.listen()
+                text = speechRecog.getSentence()
+                ev = sse.ServerSentEvent(text)
+                yield ev.encode()
+        except GeneratorExit:
+            pass
+    return Response(gen(), mimetype = "text/event-stream")
 
-@app.route("/profile")
-def profile():
-    return "profile"
 
 if __name__ == "__main__":
     app.run()
--- static/script.js
+++ static/script.js
@@ -7,3 +7,32 @@ $(document).on("click", ".dropdown-menu li", function(){
     },
   });
 });
+
+var clicked = false;
+$(document).on("click", "#start", function(){
+  alert("clicked");
+  if (!clicked) {
+    clicked = true
+    evtSrc.close();
+    evtSrc = new EventSource("/chatData/False");
+  }
+  else {
+    evtSrc.close();
+    evtSrc = new EventSource("/chatData/True");
+    clicked = false
+  }
+  // $.ajax({
+  //   type: "GET",
+  //   url: "/language/" + $(this).text(),
+  //   success: function(data) {
+  //     alert(data);
+  //   },
+  // });
+});
+
+var evtSrc = new EventSource("/chatData/True");
+
+evtSrc.onmessage = function(e) {
+  var previousText = $(".chatText").html();
+  $(".chatText").html(previousText + "<br>" + e.data);
+}
